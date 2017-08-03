74144d52388b74596599c6c73d97412e256b1031
xi <xi8@kth.se>
2017-08-03 10:52:37 +0200

    first commit

---
+++ trpo_test.py
@@ -0,0 +1,50 @@
+from sandbox.rocky.tf.algos.trpo import TRPO
+# from rllab.algos.trpo import TRPO
+from rllab.baselines.linear_feature_baseline import LinearFeatureBaseline
+from rllab.envs.normalized_env import normalize
+from sandbox.rocky.tf.policies.categorical_mlp_policy import CategoricalMLPPolicy
+# from rllab.policies.categorical_mlp_policy import CategoricalMLPPolicy
+from rllab.misc.instrument import run_experiment_lite
+
+import joblib
+from environment import env_vrep
+
+# def run_task(*_):
+env = normalize(env_vrep.Simu_env(20000))
+# env = env_vrep.Simu_env(20000)
+
+policy = CategoricalMLPPolicy(
+    name = 'policy',
+    env_spec=env.spec,
+    hidden_sizes=(32, 32)
+)
+
+# policy = joblib.load('data/policy_params.pkl')
+
+baseline = LinearFeatureBaseline(env_spec=env.spec)
+
+algo = TRPO(
+    env=env,
+    policy=policy,
+    baseline=baseline,
+    batch_size=500,
+    max_path_length=40,
+    n_itr=500,
+    discount=0.99,
+    # plot = True
+    # step_size=0.05,
+)
+algo.train()
+
+# run_experiment_lite(
+#     run_task,
+#     # Number of parallel workers for sampling
+#     n_parallel=1,
+#     # Only keep the snapshot parameters for the last iteration
+#     snapshot_mode="last",
+#     # Specifies the seed for the experiment. If this is not provided, a random seed
+#     # will be used
+#     seed=1,
+#     # plot=True,
+# )
+
